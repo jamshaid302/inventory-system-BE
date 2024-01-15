843de6374b2e2bad0c5c09f9e03f9b735361b962
M Jamshaid Khan <jkniazi302@gmail.com>
2024-01-14 18:35:37 -0800

    - send message through socket on every 10 second cron when the each of the item out of stock

--- src/cron/index.ts
+++ src/cron/index.ts
@@ -1,9 +1,19 @@
 import cron from 'node-cron';
+import prisma from '../../prisma';
 class CronHandler {
   mainCronHandler = async (io: any) => {
-    cron.schedule('*/10 * * * * *', () => {
-      //   io.emit('message', 'Welcome to the server!');
-      console.log('cron runs successfully');
+    cron.schedule('*/10 * * * * *', async () => {
+      const products = await prisma.products.findMany({});
+
+      if (products?.length) {
+        products?.forEach((item) => {
+          if (item?.quantity <= 5) {
+            console.log('cron runs successfully', item);
+            // emit a notificattion of low quantity
+            io.emit('message', `${item?.itemName} is low in stock`);
+          }
+        });
+      }
     });
   };
 }
